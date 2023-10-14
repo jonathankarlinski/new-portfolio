16ea6cdbbf3e4872cfa82cfd9485307a0f041720
Jonathan <jonathankarlinski57@gmail.com>
2023-10-13 21:10:49 -0300

    feat: Adicionado Vercel Analytics ao projeto

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import '@/styles/main.scss';
-
 import { AppProvider } from '../context/AppContext';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <>
+      <AppProvider>
+        <Component {...pageProps} />
+        <Analytics />
+      </AppProvider>
+    </>
   )
 }
